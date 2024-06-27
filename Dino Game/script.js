06c6352f871c71ae87a7ca32464b1cff52a56753
Qardash <161454530+QardashAliyev@users.noreply.github.com>
2024-06-28 01:30:54 +0400

    Add files via upload

---
+++ Dino Game/script.js
@@ -0,0 +1,38 @@
+//let dino = document.querySelector("#dino");
+//let cactus = document.querySelector("#cactus");
+console.log(dino);
+
+
+
+// function zipla() {
+
+    
+//     if (cactus.classList != "cactus-animate") {
+//         cactus.classList.add("cactus-animate");
+//     }
+//     if (dino.classList != "dino-animate") {
+//         dino.classList.add("dino-animate");
+//         setTimeout(function () {
+//             dino.classList.remove("dino-animate");
+
+//         }, 600)
+
+       
+//     }
+// }
+
+// var deymeKontrolEtme = setInterval(function () {
+
+//     var dinoBottom = parseInt(window.getComputedStyle(dino)
+//         .getPropertyValue("bottom"));
+
+
+//     var cactusLeft = parseInt(window.getComputedStyle(cactus)
+//         .getPropertyValue("left"));
+//     if (cactusLeft > 0 && cactusLeft < 50 && dinoBottom < 30) {
+//         alert("OYUN BITDI");
+//     }
+
+
+// }, 15);
+
